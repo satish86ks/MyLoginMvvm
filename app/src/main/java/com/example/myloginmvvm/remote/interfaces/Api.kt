769963682e89ac223e050c8b647d02536665f55a
satishkumarsingh <satish86ks@gmail.com>
2023-09-05 02:04:28 -0700

    update api

--- app/src/main/java/com/example/myloginmvvm/remote/interfaces/Api.kt
+++ app/src/main/java/com/example/myloginmvvm/remote/interfaces/Api.kt
@@ -27,27 +27,7 @@ interface Api {
         @Header("Authorization") code: String
     ): Response<UserListResponse>
 
-//    @POST("pudmobilebff/api/v1/bff/bffuserBookingList")
-//    suspend fun getLoginDetails(
-//        @Header("Authorization") code: String,
-//        @Body requestBody: RequestBody?
-//    ): Response<UserResponse>
 
-//    @GET("umeshbs/mobileservice/0.1/api/v1/trip/management/bookings/{userId}/")
-//    suspend fun getDetails(
-//        @Path("userId") id: String,
-//        @Query("bookingStatus") bookingStatus: String,
-//        @Query("bookingDate") bookingDate: String
-//    ): Response<BookingResponseNew>
-//
-//    @Multipart
-//    @POST("agenda/digitalinventory/api/v1/images/upload")
-//    fun uploadImage(
-//        @Header("Authorization") code: String,
-//        @Part file: MultipartBody.Part,
-//        @Part("partFileName") partFileName:RequestBody,
-//        @Part("bucket") bucket:RequestBody
-//    ): Call<ImageUploadResponse>
 
     @HTTP(method = "DELETE", path = "agenda/digitalinventory/api/v1/images/upload", hasBody = true)
     fun deleteImage(
