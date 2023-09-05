bd3a5981ba6c060546d4ae582915593f8615d531
satishkumarsingh <satish86ks@gmail.com>
2023-09-05 00:56:07 -0700

    update

--- app/src/main/java/com/example/myloginmvvm/adapter/ListViewAdapter.kt
+++ app/src/main/java/com/example/myloginmvvm/adapter/ListViewAdapter.kt
@@ -5,9 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myloginmvvm.R
 import com.example.myloginmvvm.data.Data
